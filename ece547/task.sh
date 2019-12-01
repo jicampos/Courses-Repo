e2ca70b97f96b904a8c022e4d9b4c186bfe91614
jicampos <javic9788@gmail.com>
2019-12-01 03:48:24 -0600

    changed websites and output display

--- ece547/task.sh
+++ ece547/task.sh
@@ -4,7 +4,7 @@
 dt=$(date '+%m/%d/%Y,%H:%M:%S')
 file_ping="ping.txt"
 file_traceroute="traceroute.txt"
-websites="www.purdue.edu www.rose-hulman.edu www.nyu.edu www.stonybrook.edu www5.usp.br www.unicamp.br www.cam.ac.uk www.iitd.ac.in www.iitb.ac.in" 
+websites="www.purdue.edu www.rose-hulman.edu www.pace.edu www.princeton.edu www5.usp.br www.unicamp.br www.cam.ac.uk www.iitd.ac.in www.amu.ac.in" 
 
 printf "$dt" >> $file_ping
 printf "$dt" >> $file_traceroute
@@ -14,14 +14,9 @@ printf "Beginning to ping...\n\n"
 for website in $websites
 do
 	printf "$website" 
-	STRING=",$(ping -c 12 $website | tail -1| awk -F '/' '{print $5}')"
-	if [ $STRING = "," ]; then
-		printf ",0" >> $file_ping
-		printf ",0\n"
-	else 
-		printf "$STRING" >> $file_ping
-		printf "$STRING\n"
-	fi
+	STRING="$(ping -c 12 $website | tail -1| awk -F '/' '{print $5}')"
+	printf ",$STRING" >> $file_ping
+	printf ": $STRING\n"
 done
 
 printf "\n" >> $file_ping 
@@ -32,7 +27,7 @@ do
 	printf "$website" 
 	OUTPUT="$(traceroute $website | tail -1 | awk -F ' ' '{print $1}')"
 	printf ",$OUTPUT" >> $file_traceroute
-	printf ",$OUTPUT\n"
+	printf ": $OUTPUT\n"
 done 
 
 printf "\n" >> $file_traceroute 
